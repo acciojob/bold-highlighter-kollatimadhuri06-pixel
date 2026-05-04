a876d10048d109efdaf923e63519619cd5ee4a2c
kollatimadhuri06-pixel <kollatimadhuri06@gmail.com>
2026-05-04 11:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function highlight() {
-    //Write your code here
 
-
-}
-
-
-function return_normal() {
-    //Write your code here
-
+    //Write your code here<script>
+    let target =document.querySelectorAll("strong")
+        function highlight(){
+            for(let text of target){
+                text.style.color="green"
+            }
+		}
     
-}
+        
+        function return_normal(){
+            for(let text of target){
+                text.style.color ="black"
+            }
+
+        }
+
+
+
+
+
+
